c882932c63f605feca021ef999d8f48a15296e56
Kristopher Lindsey <kristyle1227@yahoo.com>
2016-05-05 15:24:57 -0400

    Added description to project.clj file. Added github url to website url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject file-system-programming "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "FIXME: Read and write data to file system"
+  :url "https://github.com/kris1226/file-system-programmin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
