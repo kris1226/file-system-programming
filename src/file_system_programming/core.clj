ac996003177de0c1618cb319af46a822d87e9469
Kristopher Lindsey <kristyle1227@yahoo.com>
2016-05-30 23:09:14 -0400

    Added save-data and load-data functions along with a atom

--- src/file_system_programming/core.clj
+++ src/file_system_programming/core.clj
@@ -1,8 +1,18 @@
 (ns file-system-programming.core
 (use clojure.java.io)
   (:gen-class))
+(import 'java.io.File)
+
+(def db (atom {:key "value"}))
+
+(defn save-data []
+  (spit "somefile" (prn-str @db)))
+
+(defn load-data []
+  (reset! db (read-string (slurp "somefile"))))
 
 (defn read-file [path]
   (with-open [rdr (reader path)]
     (doseq [line (line-seq rdr)]
       (println line))))
+
