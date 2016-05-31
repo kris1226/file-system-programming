951d2bfc0e3bef836d85cf7077c08db1aa5d8c68
Kristopher Lindsey <kristyle1227@yahoo.com>
2016-05-30 23:25:05 -0400

    save-agent along with save-data-2 function

--- src/file_system_programming/core.clj
+++ src/file_system_programming/core.clj
@@ -3,13 +3,22 @@
   (:gen-class))
 (import 'java.io.File)
 
-(def db (atom {:key "value"}))
+(def db (atom {:magician-name "Kristopher"}))
+
+(def save-agent (agent nil))
+
+(defn save-data-2 []
+  (send-off save-agent
+    (fn [_]
+      (spit "somefile.tmp" (prn-str @db))
+      (.renameTo (File. "somefile.tmp") (File. "somefile")))))
 
 (defn save-data []
-  (spit "somefile" (prn-str @db)))
+  (spit "somefile.txt" (prn-str @db)))
 
 (defn load-data []
   (reset! db (read-string (slurp "somefile"))))
+
 
 (defn read-file [path]
   (with-open [rdr (reader path)]
